2ea128bfba8913c4ea407df7864f7bcd31911534
Irek Skrobis <irek.skrobis@gmail.com>
2021-07-12 22:00:36 +0200

    exercise 3 - answer

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,15 @@
 class Post < ApplicationRecord
   has_rich_text :content
 
+  before_save :set_plain_text_body
+
   scope :search, -> (query) {
-    joins(:rich_text_content).merge(ActionText::RichText.where('body ILIKE ?', "%#{query}%"))
+    joins(:rich_text_content).merge(ActionText::RichText.where('plain_text_body ILIKE ?', "%#{query}%"))
   }
+
+  private
+
+  def set_plain_text_body
+    content.plain_text_body = content.body.to_plain_text
+  end
 end
